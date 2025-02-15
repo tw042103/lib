2eba516c84f096581ca2138847a0f97b137c3734
“tw042103” <1091105594@qq.com>
2025-02-15 17:13:11 +0800

    update

---
+++ LearnOpencv/1/c++/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\opencv.hpp>
+using namespace std;
+using namespace cv;
+
+
+int main() {
+    // 读取图像
+    cv::Mat image = cv::imread("cqss.png"); // 替换为你的图像路径
+    if (image.empty()) {
+        std::cerr << "Error: Could not open or find the image!" << std::endl;
+        return -1;
+    }
+
+    // 创建一个全屏窗口
+    cv::namedWindow("FullScreen Image", cv::WINDOW_NORMAL);
+    cv::setWindowProperty("FullScreen Image", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+
+    // 显示图像
+    cv::imshow("FullScreen Image", image);
+
+    // 等待按键
+    cv::waitKey(0);
+
+    // 关闭窗口
+    cv::destroyAllWindows();
+    return 0;
+}
